26fa96b4c0c4eeaa2a58f215b82fae29011442bb
Tim Schwartz <tim.schwartz@imperiansystems.com>
2023-09-04 15:55:33 -0500

    version 1.1.3

---
+++ types/index.ts
@@ -0,0 +1,102 @@
+export module atls {
+  export interface BaseEntity {
+    partitionKey: string;
+    rowKey: string;
+    timestamp: string;
+  }
+
+  export interface JobsEntity extends BaseEntity {
+    status: string;
+    conversation_id: string;
+    message_id: string;
+    blob_name: string;
+  }
+
+  export interface ChunksEntity extends BaseEntity {
+    job_id: string;
+    start: number;
+    end: number;
+    text: string;
+    title: string;
+    status: string;
+    conversation_id: string;
+    message_id: string;
+    user_id: string;
+    summary: string;
+  }
+
+  export interface ContextEntity extends BaseEntity {
+    conversationId: string;
+    messageId: string;
+    embeddings: string;
+    originalText: string;
+    start: number;
+    end: number;
+  }
+  
+  export interface UserEntity extends BaseEntity {
+    email: string;
+    model: string;
+    tokens: number;
+    name: string;
+    conversationId: string;
+    paymentLink?: string;
+    paymentLinkId?: string;
+  }
+  
+  export interface TokenEntity extends BaseEntity {
+    completionId: string;
+    model: string;
+    promptTokens: number;
+    completionTokens: number;
+    totalTokens: number;
+    userId: string;
+  }
+  
+  export interface ConversationEntity extends BaseEntity {
+    characterId: string;
+    title: string;
+    userId: string;
+  }
+  
+  export interface CharacterEntity extends BaseEntity {
+    name: string;
+    summary: string;
+  }
+  
+  export interface MessageEntity extends BaseEntity {
+    userId: string;
+    characterId: string;
+    conversationId: string;
+    sender: string;
+    content: string;
+    files?: string;
+    status?: string;
+  }
+
+  export type TableEntity = 
+  | UserEntity
+  | TokenEntity
+  | ConversationEntity
+  | CharacterEntity
+  | MessageEntity
+  | ContextEntity
+  | ChunksEntity
+  | JobsEntity;
+
+  export const tableNames = [
+    "Users",
+    "Conversations",
+    "Characters",
+    "Messages",
+    "Tokens",
+    "Contexts", 
+    "Chunks",
+    "Jobs",
+    "CharacterSubscriptions"
+  ] as const;
+
+  export type TableName = typeof tableNames[number];
+}
+
+export default atls;
